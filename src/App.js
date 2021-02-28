ce5a2480953d6e6e99e4ffb282a7e404d8378670
Somelina Obiechina <ifeasome@gmail.com>
2021-02-28 01:05:50 -0600

    navbar functionality added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
