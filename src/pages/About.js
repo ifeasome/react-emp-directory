94ede8fb0d18d1955db4c03a65df232c2f7c4862
Somelina Obiechina <ifeasome@gmail.com>
2021-03-03 21:43:38 -0600

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ import Col from "../components/Col";
 function About() {
   return (
     <div>
-      <Hero backgroundImage="https://unsplash.com/photos/Vr_yaCXzLP0">
+      <Hero backgroundImage="https://source.unsplash.com/Vr_yaCXzLP0/1600x900">
         <h1>Employee Directory</h1>
         <h2>The Directory for all your Employees</h2>
       </Hero>
@@ -20,17 +20,11 @@ function About() {
         <Row>
           <Col size="md-12">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+              This is the place to be. This Directory gives you all the Lego Employees of your company. 
+              No questions asked, The Directory is the most accurate you will ever see around. Click onto the next 
+              page and see for yourself. <br/> Please enjoy your React tour of The Directory. 
+              From all of us, <br/> 
+              Have a reactful day!
             </p>
           </Col>
         </Row>
